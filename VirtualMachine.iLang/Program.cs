b25019ab2e6e5e67a9d0ae95b0bd41be8f5bff60
Demuirgos <ayman.bouchareb@outlook.fr>
2024-07-23 01:16:51 +0100

    * Added both modes Examples to program.cs

--- VirtualMachine.iLang/Program.cs
+++ VirtualMachine.iLang/Program.cs
@@ -1,21 +1,13 @@
-﻿using iLang.Compilers;
+﻿#define REGISTER
+using iLang.Compilers;
 using iLang.Parsers;
 using System;
 using VirtualMachine.Builder;
+#if REGISTER 
 using VirtualMachine.Example.Register;
-using VirtualMachine.Example.Stack;
 using VirtualMachine.Processor;
 
 var code = @"
-SumRangeLoop : [a, b] => {
-    var sum <- 0;
-    while (a < b) do {
-        sum <- sum + a;
-        a <- a + 1;
-    }
-    return sum;
-}; 
-
 SumRangeRec : [a, b] => {
     if (a = b) then {
         return 0;
@@ -33,3 +25,26 @@ IVirtualMachine<Registers> vm = new VirtualMachine.Example.Register.VirtualMachi
 Console.WriteLine(AssemblyBuilder<Registers>.Disassemble(program));
 vm.LoadProgram(program);
 Console.WriteLine(vm.Run());
+#else 
+using VirtualMachine.Example.Stack;
+using VirtualMachine.Processor;
+
+var code = @"
+SumRangeRec : [a, b] => {
+    if (a = b) then {
+        return 0;
+    } else {
+        return a + SumRangeRec(a + 1, b);
+    }
+}; 
+
+Main: SumRangeRec(0, 11);
+";
+
+Parsers.ParseCompilationUnit(code, out var function);
+byte[] program = iLang.Compilers.StacksCompiler.Compiler.Compile(function);
+IVirtualMachine<Stacks> vm = new VirtualMachine.Example.Stack.VirtualMachine();
+Console.WriteLine(AssemblyBuilder<Stacks>.Disassemble(program));
+vm.LoadProgram(program);
+Console.WriteLine(vm.Run());
+#endif
