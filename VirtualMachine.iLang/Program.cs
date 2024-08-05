42abef448d882ca9eda77bdd6042193d624dc9e4
Demuirgos <ayman.bouchareb@outlook.fr>
2024-08-06 00:23:11 +0100

    add console argument mode

--- VirtualMachine.iLang/Program.cs
+++ VirtualMachine.iLang/Program.cs
@@ -8,61 +8,81 @@ using VirtualMachine.Example.Register;
 using VirtualMachine.Example.Stack;
 using VirtualMachine.iLang.Extras;
 using VirtualMachine.Processor;
+using VirtualMachine.TypeDefs.Processor;
 
-var code = @"
-SumRangeRec : [a, b] => {
-    if (a = b) then {
-        return 0;
-    } else {
-        return a + SumRangeRec(a + 1, b);
-    }
-}; 
-
-SumRangeLoop : [a, b] => {
-    var sum <- 0;
-    while (a < 100) do {
-        if (a = b) then {
-            return sum;
-        } else {
-            sum <- sum + a;
-            a <- a + 1;
-        }
-    }
-    return sum;
-};
-
-Main: SumRangeLoop(0, 11);
-";
-Parsers.ParseCompilationUnit(code, out var function);
-InterpreterExample(new Timer<Stopwatch>(), function);
-static void InterpreterExample(Timer<Stopwatch> watch, iLang.SyntaxDefinitions.CompilationUnit function)
+// get mode and file path from command line arguments
+if (args.Length < 2)
 {
-    Value result = iLang.Interpreter.Interpreter.Interpret(function, watch);
-    Console.WriteLine(result);
+    Console.WriteLine("Usage: VirtualMachine.iLang.exe <mode> <file> [-t]");
+    return;
 }
 
-static void RegisterExample(Timer<Stopwatch> watch, iLang.SyntaxDefinitions.CompilationUnit function)
+var tokens = args[0].Split(' ').Select(s => s.Replace("-", string.Empty)).ToList();
+string[] modes = ["i", "cr", "cs"];
+
+var mode = tokens.Find(word => modes.Contains(word));
+var filePath = args[1];
+var shouldTime = tokens.Contains("t");
+var shouldDisassemble = tokens.Contains("d");
+var shouldTrace = tokens.Contains("tr");
+
+
+// read the file content
+var code = System.IO.File.ReadAllText(filePath);
+
+
+if (!modes.Contains(mode))
+{
+    Console.WriteLine("Invalid mode");
+    return;
+}
+
+Parsers.ParseCompilationUnit(code, out var function);
+
+ITimer<Stopwatch> watch = shouldTime ? NullTimer<Stopwatch>.Instance : new Timer<Stopwatch>();
+switch (mode)
+{
+    case "-i":
+        InterpreterRun(watch, function);
+        break;
+    case "-cr":
+        RegisterRun(watch, function);
+        break;
+    case "-cs":
+        StackRun(watch, function);
+        break;
+    default:
+        Console.WriteLine("Invalid mode");
+        break;
+
+}
+static object InterpreterRun(ITimer<Stopwatch> watch, iLang.SyntaxDefinitions.CompilationUnit function)
+{
+    Value result = Interpreter.Interpret(function, watch);
+    return result;
+}
+
+static object RegisterRun(ITimer<Stopwatch> watch, iLang.SyntaxDefinitions.CompilationUnit function)
 {
     var tracer_r = new Tracer<Registers>();
     byte[] program_r = iLang.Compilers.RegisterTarget.Compiler.Compile(function);
-
-    Console.WriteLine(VirtualMachine.Builder.AssemblyBuilder<Registers>.Disassemble(program_r));
 
     IVirtualMachine<Registers> vm_r = new VirtualMachine.Example.Register.VirtualMachine();
     vm_r.LoadProgram(program_r);
 
     _ = vm_r.Run(tracer_r, watch);
+    return vm_r.State.Holder[0];
 }
 
-static void StackExample(Timer<Stopwatch> watch, iLang.SyntaxDefinitions.CompilationUnit function)
+static object StackRun(ITimer<Stopwatch> watch, iLang.SyntaxDefinitions.CompilationUnit function)
 {
     var tracer_s = new Tracer<Stacks>();
     byte[] program_s = iLang.Compilers.StacksCompiler.Compiler.Compile(function);
-    
-    Console.WriteLine(VirtualMachine.Builder.AssemblyBuilder<Stacks>.Disassemble(program_s));
 
     IVirtualMachine<Stacks> vm_s = new VirtualMachine.Example.Stack.VirtualMachine();
     vm_s.LoadProgram(program_s);
 
     _ = vm_s.Run(tracer_s, watch);
+
+    return vm_s.State.Holder.Operands.LastOrDefault();
 }
